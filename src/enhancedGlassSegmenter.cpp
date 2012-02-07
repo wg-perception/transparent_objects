2b642958ce3c7c8bbcee45d29cf7b5666fce0a86
Ilya Lysenkov <lysenkov.ilya@gmail.com>
2012-02-07 15:05:14 +0400

    Finished the ECA classifier

--- src/enhancedGlassSegmenter.cpp
+++ src/enhancedGlassSegmenter.cpp
@@ -36,6 +36,7 @@ Region::Region(const cv::Mat &_image, const cv::Mat &_mask)
   clusterIntensities();
 }
 
+
 const cv::Mat& Region::getColorHistogram() const
 {
   return hist;
@@ -142,19 +143,17 @@ void computeColorSimilarity(const Region &region_1, const Region &region_2, floa
 void computeOverlayConsistency(const Region &region_1, const Region &region_2, float &slope, float &intercept)
 {
   //TODO: move up
-  const float minAlpha = 0.0f;
+  const float minAlpha =-0.001f;
   const float maxAlpha = 1.001f;
-
 
   Mat clusters_1 = region_1.getIntensityClusters();
   Mat clusters_2 = region_2.getIntensityClusters();
 
-  Mat b = clusters_1;
+  Mat b = clusters_1.clone();
   const int dim = 2;
   Mat A = Mat(b.rows, dim, b.type());
-  Mat clusters_2_col = clusters_2;
   Mat col_0 = A.col(0);
-  clusters_2_col.copyTo(col_0);
+  clusters_2.copyTo(col_0);
   A.col(1).setTo(1.0);
 
   Mat model;
@@ -163,11 +162,17 @@ void computeOverlayConsistency(const Region &region_1, const Region &region_2, f
   CV_Assert(model.total() == dim);
   if (model.at<float>(0) < minAlpha || model.at<float>(0) > maxAlpha)
   {
-    //TODO: test this
-    std::swap(col_0, b);
+    b = clusters_2.clone();
+    clusters_1.copyTo(col_0);
     solve(A, b, model, DECOMP_SVD);
   }
-  CV_Assert(model.at<float>(0) >= minAlpha && model.at<float>(0) <= maxAlpha);
+  if (model.at<float>(0) < minAlpha || model.at<float>(0) > maxAlpha)
+  {
+    cout << A << endl;
+    cout << b << endl;
+    cout << model << endl;
+    CV_Error(CV_StsError, "Cannot estimate overlay consistency");
+  }
 
   slope = model.at<float>(0);
   intercept = model.at<float>(1);
@@ -176,9 +181,9 @@ void computeOverlayConsistency(const Region &region_1, const Region &region_2, f
 void oversegmentImage(const cv::Mat &image, cv::Mat &segmentation)
 {
   //TODO: move up
-  const float sigma = 0.5f;
+  const float sigma = 0.2f;
   const float k = 500.0f;
-  const int min_size = 50;
+  const int min_size = 200;
 
   const string sourceFilename = "imageForSegmenation.ppm";
   const string outputImageFilename = "segmentedImage.ppm";
@@ -190,7 +195,7 @@ void oversegmentImage(const cv::Mat &image, cv::Mat &segmentation)
   command << "./segment " << sigma << " " << k << " " << min_size << " " << sourceFilename << " " << outputImageFilename;
 
   std::system(command.str().c_str());
-  sleep(3);
+  sleep(2);
 
   segmentation.create(image.size(), CV_32SC1);
   std::ifstream segmentationTxt(outputTxtFilename.c_str());
@@ -205,7 +210,7 @@ void oversegmentImage(const cv::Mat &image, cv::Mat &segmentation)
   segmentationTxt.close();
 }
 
-void showSegmentation(const cv::Mat &segmentation)
+void showSegmentation(const cv::Mat &segmentation, const std::string &title)
 {
   CV_Assert(segmentation.type() == CV_32SC1);
   Vec3b *colors = new Vec3b[segmentation.total()];
@@ -223,11 +228,12 @@ void showSegmentation(const cv::Mat &segmentation)
     }
   }
 
-  imshow("oversegmentation", image);
+  imshow(title, image);
   waitKey();
 }
 
-void segmentation2regions(const cv::Mat &image, const cv::Mat &segmentation, vector<Region> &regions)
+
+void segmentation2regions(const cv::Mat &image, cv::Mat &segmentation, vector<Region> &regions)
 {
   CV_Assert(segmentation.type() == CV_32SC1);
   vector<int> labels = segmentation.reshape(1, 1);
@@ -235,22 +241,38 @@ void segmentation2regions(const cv::Mat &image, const cv::Mat &segmentation, vec
   vector<int>::iterator endIt = std::unique(labels.begin(), labels.end());
   labels.resize(endIt - labels.begin());
 
+  int firstFreeLabel = 1 + *std::max_element(labels.begin(), labels.end());
+
   regions.clear();
   for (size_t i = 0; i < labels.size(); ++i)
   {
     Mat mask = (segmentation == labels[i]);
+    segmentation.setTo(firstFreeLabel + i, mask);
     Region currentRegion(image, mask);
     regions.push_back(currentRegion);
   }
+  segmentation -= firstFreeLabel;
 }
 
-enum {DIFFERENT, GLASS_COVERED} TrainingLabels;
+enum TrainingLabels {THE_SAME = 0, GLASS_COVERED = 1};
+
+void regions2sample(const Region &region_1, const Region &region_2, cv::Mat &sample)
+{
+  float colorDistance;
+  computeColorSimilarity(region_1, region_2, colorDistance);
+  float slope, intercept;
+  computeOverlayConsistency(region_1, region_2, slope, intercept);
+
+  const int dim = 3;
+  sample = (Mat_<float>(1, dim) << colorDistance, slope, intercept);
+}
 
 void train(CvSVM &svm)
 {
   //TODO: move up
   const string trainingFilesList = "/media/2Tb/transparentBases/rgbGlassData/trainingImages.txt";
   const string groundTruthFilesList = "/media/2Tb/transparentBases/rgbGlassData/trainingImagesGroundTruth.txt";
+  const float maxSampleDistance = 0.1f;
 
   vector<string> trainingGroundTruhFiles;
   readLinesInFile(groundTruthFilesList, trainingGroundTruhFiles);
@@ -288,28 +310,35 @@ void train(CvSVM &svm)
 
     for (size_t i = 0; i < regions.size(); ++i)
     {
-      for (size_t j = 0; j < regions.size(); ++j)
+      for (size_t j = i + 1; j < regions.size(); ++j)
       {
         if (i == j)
         {
           continue;
         }
 
-        float colorDistance;
-        computeColorSimilarity(regions[i], regions[j], colorDistance);
-        float slope, intercept;
-        computeOverlayConsistency(regions[i], regions[j], slope, intercept);
+        Mat sample;
+        regions2sample(regions[i], regions[j], sample);
 
-        const int dim = 3;
-        Mat sample = (Mat_<float>(1, dim) << colorDistance, slope, intercept);
         trainingData.push_back(sample);
+        int currentLabel;
         if (isGlass[i] ^ isGlass[j])
         {
-          trainingLabelsVec.push_back(GLASS_COVERED);
+          currentLabel = GLASS_COVERED;
         }
         else
         {
-          trainingLabelsVec.push_back(DIFFERENT);
+          currentLabel = THE_SAME;
+        }
+        trainingLabelsVec.push_back(currentLabel);
+
+        Mat symmetricSample;
+        regions2sample(regions[j], regions[i], symmetricSample);
+        if (norm(sample - symmetricSample) > maxSampleDistance)
+        {
+          //TODO: is it a correct way to process such cases?
+          trainingData.push_back(symmetricSample);
+          trainingLabelsVec.push_back(currentLabel);
         }
       }
     }
@@ -319,22 +348,114 @@ void train(CvSVM &svm)
 
   CvSVMParams svmParams;
   //TODO: move up
-  svmParams.svm_type = CvSVM::C_SVC;
+  //svmParams.svm_type = CvSVM::C_SVC;
+
+  svmParams.svm_type = CvSVM::NU_SVC;
+  svmParams.nu = 0.5;
   cout << "trainingData size: " << trainingData.rows << " x " << trainingData.cols << endl;
 
   svm.train(trainingData, trainingLabels, Mat(), Mat(), svmParams);
+
+  int wrongClassificationCount = 0;
+  for (size_t i = 0; i < trainingData.rows; ++i)
+  {
+    Mat sample = trainingData.row(i);
+    int label = cvRound(svm.predict(sample));
+    if (label != trainingLabelsVec[i])
+    {
+      ++wrongClassificationCount;
+    }
+  }
+
+  cout << "training error: " << static_cast<float>(wrongClassificationCount) / trainingData.rows << endl;
+}
+
+void visualizeClassification(const vector<Region> &regions, const vector<float> &labels, cv::Mat &visualization)
+{
+  if (visualization.empty())
+  {
+    visualization.create(regions[0].getMask().size(), CV_8UC1);
+    visualization.setTo(0);
+  }
+
+  for (size_t i = 0; i < regions.size(); ++i)
+  {
+    int currentLabel = cvRound(labels[i]);
+    if (currentLabel == GLASS_COVERED)
+    {
+      visualization.setTo(255, regions[i].getMask());
+    }
+  }
+}
+
+
+
+
+
+
+
+struct InteractiveClassificationData
+{
+  CvSVM *svm;
+  Mat segmentation;
+  vector<Region> regions;
+};
+
+void onMouse(int event, int x, int y, int, void *rawData)
+{
+  if (event != CV_EVENT_LBUTTONDOWN)
+  {
+    return;
+  }
+
+  InteractiveClassificationData *data = static_cast<InteractiveClassificationData*>(rawData);
+
+  int regionIndex = data->segmentation.at<int>(y, x);
+
+  vector<float> labels(data->regions.size());
+  for (size_t i = 0; i < data->regions.size(); ++i)
+  {
+    Mat sample;
+    regions2sample(data->regions[regionIndex], data->regions[i], sample);
+    labels[i] = cvRound(data->svm->predict(sample));
+    cout << labels[i] << endl;
+  }
+
+  Mat visualization;
+  visualizeClassification(data->regions, labels, visualization);
+  imshow("classification", visualization);
 }
 
 int main()
 {
-//  Mat testImage = imread("tea_table2.jpg");
-//  Mat oversegmentation;
-//  oversegmentImage(testImage, oversegmentation);
-//  showSegmentation(oversegmentation);
-
+  const string svmFilename = "svm.xml";
   CvSVM svm;
-  train(svm);
+//  train(svm);
+//  svm.save(svmFilename.c_str());
+  svm.load(svmFilename.c_str());
 
+  const string testSegmentationTitle = "test segmentation";
+
+  Mat testImage = imread("/media/2Tb/transparentBases/rgbGlassData/Test/plate_building.jpg");
+
+//  Mat testImage = imread("/media/2Tb/transparentBases/rgbGlassData/Training/teaB1f.jpg");
+  CV_Assert(!testImage.empty());
+
+  Mat oversegmentation;
+  oversegmentImage(testImage, oversegmentation);
+  vector<Region> regions;
+  segmentation2regions(testImage, oversegmentation, regions);
+
+  namedWindow(testSegmentationTitle);
+
+
+  InteractiveClassificationData data;
+  data.svm = &svm;
+  data.segmentation = oversegmentation;
+  data.regions = regions;
+
+  setMouseCallback(testSegmentationTitle, onMouse, &data);
+  showSegmentation(oversegmentation, testSegmentationTitle);
 
   return 0;
 }
