30fd31b8a420cc3075b7e46fe005f90dc76e801b
Ilya Lysenkov <lysenkov.ilya@gmail.com>
2012-03-20 17:53:29 +0400

    Added computation of boundary recall-precision

--- src/apps/evaluateOnDataset.py
+++ src/apps/evaluateOnDataset.py
@@ -9,6 +9,70 @@ import matplotlib.pyplot as plt
 import tempfile
 import os
 
+class Evaluator:
+
+    def __init__(self, curvePointsCount):
+        self._recallValid = [0] * curvePointsCount
+        self._recallAll = [0] * curvePointsCount
+        self._precisionValid = [0] * curvePointsCount
+        self._precisionAll = [0] * curvePointsCount
+
+        self._invertedPrecisions = [None] * curvePointsCount
+        self._recalls = [None] * curvePointsCount
+
+    def addObservation(self, pointIndex, recallValid, recallAll, precisionValid, precisionAll):
+        self._recallValid[pointIndex] += recallValid
+        self._recallAll[pointIndex] += recallAll
+        self._precisionValid[pointIndex] += precisionValid
+        self._precisionAll[pointIndex] += precisionAll
+
+    def computeStatistics(self):
+        for i in range(len(self._recalls)):
+            if self._recallAll[i] != 0:
+                self._recalls[i] = float(self._recallValid[i]) / self._recallAll[i]
+            if self._precisionAll[i] != 0:
+                self._invertedPrecisions[i] = 1.0 - float(self._precisionValid[i]) / self._precisionAll[i]
+
+    def printStatistics(self):
+        print self._invertedPrecisions
+        print self._recalls
+        for i in range(len(self._recalls)):
+            print 'recall =', self._recalls[i]
+            print '1 - precision =', self._invertedPrecisions[i]
+
+    def plot(self, title):
+        plt.clf()
+        plt.plot(self._invertedPrecisions, self._recalls, 'o-')
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.xlabel('1 - precision')
+        plt.ylabel('recall')
+        plt.title('Evaluation of Glass Segmentation Algorithms: ' + title)
+        plt.grid(True)
+        plt.legend(['RGB data'], title='Algorithm', loc='best')
+        plt.savefig('evaluation_' + title + '.png')
+#        plt.show()
+
+
+def mask2dtAndEdgelsCount(mask):
+    maskCopy = mask.copy()
+    contours, hierarchy = cv2.findContours(maskCopy, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contoursImage = np.zeros(mask.shape, np.uint8)
+    cv2.drawContours(contoursImage, contours, -1, (255))
+    edgelsCount = len(np.nonzero(contoursImage == 255)[0])
+    dt = cv2.distanceTransform(~contoursImage, cv2.cv.CV_DIST_L2, cv2.cv.CV_DIST_MASK_PRECISE)
+    return [dt, edgelsCount]
+
+def computeRecallPrecisionEdgels(groundTruthDT, segmentedImageDT):
+    maxDistance = 2
+    eps = 0.1
+
+    recallEdgelsCount = len(np.nonzero(np.logical_and(groundTruthDT < eps, segmentedImageDT < maxDistance + eps))[0])
+    precisionEdgelsCount = len(np.nonzero(np.logical_and(groundTruthDT < maxDistance + eps, segmentedImageDT < eps))[0])
+
+    return [recallEdgelsCount, precisionEdgelsCount]
+
+
 if __name__ == '__main__':
     assert len(sys.argv) == 5, sys.argv[0] + ' <testListFilename> <classifierFilename> <propagationScaligsList> <algorithmName>'
 
@@ -32,9 +96,8 @@ if __name__ == '__main__':
 
     testFilenames = open(testListFilename, 'r').read().splitlines()
     
-    allTrueGlassArea = [0] * len(segmentationTemporaryFiles)
-    allPredictedGlassArea = [0] * len(segmentationTemporaryFiles)
-    allValidPredictedGlassArea = [0] * len(segmentationTemporaryFiles)
+    areaEvaluator = Evaluator(len(segmentationTemporaryFiles))
+    boundaryEvaluator = Evaluator(len(segmentationTemporaryFiles))
 
     for imageFilename in testFilenames:
         print imageFilename
@@ -53,41 +116,38 @@ if __name__ == '__main__':
         call([segmentImage, testFolder, fullImageIndex, classifierFilename, segmentationListFilename, propagationScalingsList, algorithmName])
 
         groundTruth = cv2.imread(groundTruthFilename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        glassLabel = 255
+        groundTruthGlassMask = (groundTruth == glassLabel).astype(np.uint8)
+        [groundTruthDT, groundTruthEdgelsCount] = mask2dtAndEdgelsCount(groundTruthGlassMask)
+
         for (index, temporaryFile) in enumerate(segmentationTemporaryFiles):
             segmentationImage = cv2.imread(temporaryFile[1], cv2.CV_LOAD_IMAGE_GRAYSCALE)
+            [segmentationImageDT, segmentationImageEdgelsCount] = mask2dtAndEdgelsCount(segmentationImage)
+            [recallEdgelsCount, precisionEdgelsCount] = computeRecallPrecisionEdgels(groundTruthDT, segmentationImageDT)
 
-            glassLabel = 255
             trueGlassArea = len(np.nonzero(groundTruth == glassLabel)[0])
             predictedGlassArea = len(np.nonzero(segmentationImage == glassLabel)[0])
             validPredictedGlassArea = len(np.nonzero(np.logical_and(groundTruth == glassLabel, segmentationImage == groundTruth))[0])
-            allTrueGlassArea[index] += trueGlassArea
-            allPredictedGlassArea[index] += predictedGlassArea
-            allValidPredictedGlassArea[index] += validPredictedGlassArea
-            if predictedGlassArea != 0:
-                print 'precision =', float(validPredictedGlassArea) / predictedGlassArea
-            if trueGlassArea != 0:
-                print 'recall =', float(validPredictedGlassArea) / trueGlassArea
 
-    invertedPrecisions = [None] * len(allTrueGlassArea)
-    recalls = [None] * len(allTrueGlassArea)
-    for i in range(len(allTrueGlassArea)):
-        if allPredictedGlassArea[i] != 0:
-            invertedPrecisions[i] = 1.0 - float(allValidPredictedGlassArea[i]) / allPredictedGlassArea[i]
-        if allTrueGlassArea[i] != 0:
-            recalls[i] = float(allValidPredictedGlassArea[i]) / allTrueGlassArea[i]
-        print 'overall precision =', 1.0 - invertedPrecisions[i]
-        print 'overall recall =', recalls[i]
+            boundaryEvaluator.addObservation(index, recallEdgelsCount, groundTruthEdgelsCount, precisionEdgelsCount, segmentationImageEdgelsCount)
+            areaEvaluator.addObservation(index, validPredictedGlassArea, trueGlassArea, validPredictedGlassArea, predictedGlassArea)
 
-    plt.plot(invertedPrecisions, recalls)
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.xlabel('1 - precision')
-    plt.ylabel('recall')
-    plt.title('Evaluation of Glass Segmentation Algorithms')
-    plt.grid(True)
-    plt.legend(['RGB data'], title='Algorithm', loc='best')
-    plt.savefig('evaluation.png')
-    plt.show()
+        areaEvaluator.computeStatistics()
+        areaEvaluator.printStatistics()
+        areaEvaluator.plot('area')
+
+        boundaryEvaluator.computeStatistics()
+        boundaryEvaluator.plot('boundary')
+
+
+    areaEvaluator.computeStatistics()
+    print 'final area: '
+    areaEvaluator.printStatistics()
+    areaEvaluator.plot('area')
+
+    boundaryEvaluator.computeStatistics()
+    boundaryEvaluator.printStatistics()
+    boundaryEvaluator.plot('boundary')
 
     for segmentationFile in segmentationTemporaryFiles:
         os.close(segmentationFile[0])
