41f5e760e7135ba951cdc5ee52a497a47ce19d2b
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-22 15:25:03 -0700

    comply to the new DB API

--- python/object_recognition_transparent_objects/detector.py
+++ python/object_recognition_transparent_objects/detector.py
@@ -3,7 +3,7 @@
 Module defining the transparent objects detector to find objects in a scene
 """
 
-from object_recognition_core.db.object_db import ObjectDb, DbModels
+from object_recognition_core.db.object_db import ObjectDb, Models
 from object_recognition_core.pipelines.detection import DetectionPipeline
 from object_recognition_core.utils import json_helper
 import transparent_objects_cells
@@ -22,7 +22,7 @@ class TransparentObjectsDetectionPipeline(DetectionPipeline):
         parameters = kwargs.pop('parameters')
         object_ids = parameters['object_ids']
         object_db = ObjectDb(parameters['db'])
-        model_documents = DbModels(object_db, object_ids, self.type_name(), json_helper.dict_to_cpp_json_str(submethod))
+        model_documents = Models(object_db, object_ids, self.type_name(), json_helper.dict_to_cpp_json_str(submethod))
         registrationMaskFilename = parameters.get('registrationMaskFilename')
         return transparent_objects_cells.Detector(model_documents=model_documents, object_db=object_db,
                                             registrationMaskFilename=registrationMaskFilename, visualize=visualize)
